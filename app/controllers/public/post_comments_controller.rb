3c0c4b2d727dbe827dcdcba4646517de5f39043a
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-06-13 13:10:33 +0000

    comment

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -6,13 +6,13 @@ class Public::PostCommentsController < ApplicationController
     @comment = current_user.post_comments.new(post_comment_params)
     @comment.post_id = post.id
     @comment.save!
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
      @comment = PostComment.find(params[:id])
      @comment.destroy
-     redirect_to request.referer
+    # redirect_to request.referer
   end
 
   private
