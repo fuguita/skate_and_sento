bfcc6faedb2d7c79f7cfc792060dd9b381a4d5db
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-06-23 15:09:45 +0000

    done sento_reviews２

--- app/models/park_review.rb
+++ app/models/park_review.rb
@@ -1,5 +1,7 @@
 class ParkReview < ApplicationRecord
   belongs_to :park
   belongs_to :user
-  
+
+  validates :comment, presence:true, length: { minimum: 5, maximum: 200 }
+
 end
--- app/models/sento_review.rb
+++ app/models/sento_review.rb
@@ -1,4 +1,9 @@
 class SentoReview < ApplicationRecord
   belongs_to :sento
   belongs_to :user
+
+  validates :comment, presence:true, length: { minimum: 5, maximum: 200 }
+
+
+
 end
