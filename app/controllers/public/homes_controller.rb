acf1e432168935d63786b94658d77b15184858ea
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-06-27 05:58:18 +0000

    header

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,18 @@
 class Public::HomesController < ApplicationController
 
   def top
-    @parks = Park.star_count
+
+    # @average = Park.park_reviews.average(:star)
+    @parks = Park.all
+    @park_averages = @parks.each do |park|
+                      park.park_reviews.average(:star)
+                      end
+          # @park_averages = Park.all.order('count(star_id) desc').limit(3)
+
+                      # order('count(star_id) desc')
+
+
+    # @park_averages =
     # @park_reviews = @parks.star_count.limit(3)
   end
 
