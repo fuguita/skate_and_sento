712b184ddafbc25b96ee40c9ce440e793acf5de5
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-07-02 13:52:37 +0000

    post layout

--- app/controllers/public/park_favorites_controller.rb
+++ app/controllers/public/park_favorites_controller.rb
@@ -4,12 +4,14 @@ class Public::ParkFavoritesController < ApplicationController
     def create
       @park = Park.find(params[:park_id])
       current_user.park_favorite(@park)
+      @favorite_parks = current_user.favorite_parks
       flash.now[:notice_message] = 'お気に入りに追加しました！'
     end
 
     def destroy
       @park = current_user.park_favorites.find(params[:id]).park
       current_user.unpark_favorite(@park)
+      @favorite_parks = current_user.favorite_parks
       flash.now[:notice_message] = 'お気に入りを外しました！'
     end
 
--- app/controllers/public/sento_favorites_controller.rb
+++ app/controllers/public/sento_favorites_controller.rb
@@ -4,12 +4,14 @@ class Public::SentoFavoritesController < ApplicationController
     def create
       @sento = Sento.find(params[:sento_id])
       current_user.sento_favorite(@sento)
+      @favorite_sentos = current_user.favorite_sentos
       flash.now[:notice_message] = 'お気に入りに追加しました！'
     end
 
     def destroy
       @sento = current_user.sento_favorites.find(params[:id]).sento
       current_user.unsento_favorite(@sento)
+      @favorite_sentos = current_user.favorite_sentos
       flash.now[:notice_message] = 'お気に入りを外しました！'
     end
 
