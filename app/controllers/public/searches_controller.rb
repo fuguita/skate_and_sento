26eaa2a9b5849f4b252c4904f83a57cbf912f4cb
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-07-09 13:07:48 +0000

    almost layouts done

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,10 +4,9 @@ class Public::SearchesController < ApplicationController
         @word = params[:word]
         @model = params[:model]
           if @model == "Park"
-            @records = Park.search_for(@word)
+            @records = Park.search_for(@word).page(params[:page]).per(10).order(created_at: :desc)
           else
-            @records = Sento.search_for(@word)
+            @records = Sento.search_for(@word).page(params[:page]).per(10).order(created_at: :desc)
           end
-          
   end
 end
